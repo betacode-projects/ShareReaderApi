b9fdd24448e679dbd59345d023b2f75fe1d4f920
Azu0925 <yamaneko564783@gmail.com>
2021-06-21 14:24:57 +0900

    ルーティングとコントローラー

---
+++ api/src/controllers/file/uploadController.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const fileController = (req: express.Request, res: express.Response): void => {
+
+}
+
+export default fileController
--- api/src/routes/router.ts
+++ api/src/routes/router.ts
@@ -1,3 +1,5 @@
 import express from 'express'
+import uploadController from '../controllers/file/uploadController'
 
 export const router: express.Router = express.Router()
+router.post('/file', uploadController)
