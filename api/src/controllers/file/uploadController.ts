9385d19c58ccc30c2ce8a5922c39609e563ad82f
Azu0925 <yamaneko564783@gmail.com>
2021-06-26 03:46:34 +0900

    とりあえずできるところまで

--- api/src/controllers/file/uploadController.ts
+++ api/src/controllers/file/uploadController.ts
@@ -1,7 +1,36 @@
 import express from 'express'
+import { Storage } from '@google-cloud/storage'
 
 const fileController = (req: express.Request, res: express.Response): void => {
-  req.header
+  console.log(req.headers.authorization)
+  const bucketName = 'share-objects'
+  const filePath = ''
+  const destFileName = 'storage/'
+  const storage = new Storage()
+
+  async function uploadFile(): Promise<void> {
+    await storage.bucket(bucketName).upload(filePath, {
+      destination: destFileName,
+    })
+  
+    console.log(`${filePath} uploaded to ${bucketName}`)
+  }
+  
+  uploadFile().then(() => {
+    res.status(200).send(JSON.stringify({
+      "satus": "success",
+      "message": "Upload complete!",
+      "data": {}
+    }))
+  }).catch(() => {
+    res.status(400).send(JSON.stringify({
+      "status": "error",
+      "message": console.error,
+      "data": {}
+    }))
+  })
+
+  return
 }
 
 export default fileController
