78e0fe9aed9990e37868f6a302cdda9623c51cba
Azu0925 <yamaneko564783@gmail.com>
2021-07-05 00:26:36 +0900

    認証チェックまで

---
+++ api/src/controllers/downloadController.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const downloadController = (req: express.Request, res: express.Response): void => {
+  return
+}
+
+export default downloadController
--- api/src/routes/router.ts
+++ api/src/routes/router.ts
@@ -2,7 +2,12 @@ import express from 'express'
 import multer from 'multer'
 import isMatchedSenderAuth from '../authentication/isMutchedSenderAuth'
 import uploadController from '../controllers/file/uploadController'
+import downloadController from '../controllers/downloadController'
+import isMatchedReceiverAuth from '../middleware/isMutchedReceiverAuth'
 
 export const router: express.Router = express.Router()
 
-router.post('/file', isMatchedSenderAuth, multer({dest: 'storage/'}).single('file'), uploadController)
+const path = '/v1/api'
+
+router.post(path + '/file', isMatchedSenderAuth, multer({dest: 'storage/'}).single('file'), uploadController)
+router.get(path + '/file', isMatchedReceiverAuth, downloadController)
