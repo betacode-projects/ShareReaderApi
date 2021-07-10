8fd28cceeaf674d56cecbb89f0115f28c823606b
betacode-projects <betacode-projects.net@gmail.com>
2021-07-11 02:13:53 +0900

    送信者ユーザー削除・複数個所のバグ修正

--- api/src/modules/APIJson.ts
+++ api/src/modules/APIJson.ts
@@ -1,16 +1,14 @@
-export enum Status {
-    Success = 'success',
-    Error = 'error'
-}
+import { ApiStatus, TokenMode } from './flags'
+
 
 export class APIJson {
-    private _status: Status = Status.Error
+    private _status: ApiStatus = ApiStatus.Error
     private _message: string = ''
     private _private_token: string = ''
     private _public_token: string = ''
     private _data: {[key: string]: string} = {}
 
-    public set status(value: Status) {
+    public set status(value: ApiStatus) {
         this._status = value
     }
 
@@ -34,7 +32,7 @@ export class APIJson {
         if (this._private_token.length === 32)
             this._data['private_token'] = this._private_token
         if (this._public_token.length === 32)
-            this._data['public_token'] = this._private_token
+            this._data['public_token'] = this._public_token
 
         return JSON.stringify({
             status: this._status,
--- api/src/modules/TokenManager.ts
+++ api/src/modules/TokenManager.ts
@@ -1,6 +1,6 @@
-import DBConfig from '../database/DBConfig'
+import DBConfig from '../database/dbConfig'
 import { initModels } from '../models/init-models'
-import { UserMode, FlagID } from './flags'
+import { UserMode, FlagID, TokenMode } from './flags'
 import crypto from 'crypto'
 
 
@@ -8,19 +8,27 @@ export class TokenManager {
     private _private_token: string = ''
     private _public_token: string = ''
 
-    public async checkToken(user_mode: UserMode, bearer_auth?: string): Promise<any> {
-        if (typeof bearer_auth === 'undefined') return
-    
-        const tokens: string[] = bearer_auth.split(' ')
-        if (tokens.length === 2 && tokens[0].toLowerCase() !== 'bearer') return
-
+    public async checkToken(user_mode: UserMode, bearer_auth?: string, token_mode:TokenMode = TokenMode.Private): Promise<any> {
         return new Promise<void>((resolve, reject) => {
+            if (typeof bearer_auth === 'undefined') {
+                reject()
+                return
+            }
+    
+            const tokens: string[] = bearer_auth.split(' ')
+            if (tokens.length === 2 && tokens[0].toLowerCase() !== 'bearer') {
+                reject()
+                return
+            }
+
             initModels(DBConfig).user.findOne({
                 where: {
-                    user_private_token: tokens[1]
+                    [token_mode]: tokens[1],
+                    user_mode_id: user_mode,
+                    flag_id: FlagID.Working,
                 }
             }).then((record) => {
-                if(record?.user_mode_id !== user_mode){
+                if (typeof record?.user_private_token === 'undefined' || typeof record?.user_public_token === 'undefined'){
                     reject()
                     return
                 }
@@ -36,8 +44,33 @@ export class TokenManager {
         })
     }
 
+    public async deleteToken(user_mode: UserMode, bearer_auth?: string): Promise<any> {
+        return new Promise<void>((resolve, reject) => {
+            if (typeof bearer_auth === 'undefined') {
+                reject()
+                return
+            }
+            
+            const tokens: string[] = bearer_auth.split(' ')
+            if (tokens.length === 2 && tokens[0].toLowerCase() !== 'bearer') {
+                reject()
+                return
+            }
+
+            initModels(DBConfig).user.update({
+                flag_id: FlagID.LogicalDeletion
+            },
+            {
+                where: { 'user_private_token': tokens[1], 'user_mode_id': user_mode }
+            }).then((result) => {
+                if (result[0] === 0) reject()
+                resolve()
+            }).catch(() => reject())
+        })
+    }
+
     public generateRandom(digit: number): string {
-        const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        const S = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_"
         return Array.from(crypto.randomFillSync(new Uint8Array(digit))).map((n) => S[n % S.length]).join('')
     }
 
@@ -48,7 +81,6 @@ export class TokenManager {
 
     public async insertUserToken(user_mode: UserMode, remote_addr?: string, user_agent_data?: string): Promise<any>{
         this.generateToken()
-        console.log('insert')
 
         return initModels(DBConfig).user.create({
             flag_id: FlagID.Working,
