c4bb2870a964b548a56cac3acf8604f3c33afee0
betacode-projects <betacode-projects.net@gmail.com>
2021-07-12 00:51:09 +0900

    不要箇所削除

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,11 +6,6 @@ const app: express.Express = express()
 
 app.use(cors())
 app.use('/public', express.static(__dirname + '/public'))
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Conntent-Type, Accept")
-  next()
-})
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
