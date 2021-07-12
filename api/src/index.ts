7dece3dad225833216acb2fa54d097b6cac70c85
hirosuke-pi <31305419+betacode-projects@users.noreply.github.com>
2021-07-12 09:55:21 +0900

    Cors (#26)
    * cors
    
    * 不要箇所削除
    
    Co-authored-by: betacode-projects <betacode-projects.net@gmail.com>

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import { router } from './routes/router'
 
 const app: express.Express = express()
 
+app.use(cors())
 app.use('/public', express.static(__dirname + '/public'))
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Conntent-Type, Accept")
-  next()
-})
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(router)
--- api/src/routes/router.ts
+++ api/src/routes/router.ts
@@ -6,12 +6,14 @@ import downloadCheckParams from '../middleware/downloadCheckParams'
 import getObjectInfo from '../middleware/getObjectInfo'
 import downloadObject from '../middleware/downloadObject'
 import downloadResponse from '../middleware/downloadResponse'
+import generateToken from '../controllers/token/generateUploaderToken'
+import deleteSenderToken from '../controllers/user/deleteSenderToken'
 export const router: express.Router = express.Router()
 const path = '/v1/api'
 
 router.post(path + '/file', isMatchedSenderAuth, multer({dest: 'storage/'}).single('file'), uploadController)
 router.get(path + '/file', downloadCheckParams, getObjectInfo, downloadObject, downloadResponse)
-router.post('/file', isMatchedSenderAuth, multer({dest: 'storage/'}).single('file'), uploadController)
-router.post('/token', generateToken)
-router.delete('/user', deleteSenderToken)
+router.post(path + '/file', isMatchedSenderAuth, multer({dest: 'storage/'}).single('file'), uploadController)
+router.post(path + '/token', generateToken)
+router.delete(path + '/user', deleteSenderToken)
 
